4ee9c0eeb3f3ec8eed75a058c89d7682f3beb117
Victor Rudkovsky <vpcomtek@gmail.com>
2021-12-29 20:10:11 +0200

    first commit

---
+++ src/main/java/HelloWorldApp.java
@@ -0,0 +1,11 @@
+public class HelloWorldApp {
+    public static void main(String[] args) {
+//        echo "# 00_lesson" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git branch -M main
+//        git remote add origin git@github.com:vrudkovsky/00_lesson.git
+//        git push -u origin main
+    }
+}
